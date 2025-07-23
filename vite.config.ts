3719138890138625b60d1563eeaa223072cf12a4
Mihir Desai <mihir@mihirdesai.net>
2025-07-23 12:53:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'https://designdefinitions.in',
   server: {
     host: '::',
     port: 8080,
